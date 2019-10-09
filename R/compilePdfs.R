cdc9be9e8d7101bf0f4d57f3c70cbdd60e7d900f
Are Edvardsen <biorakel@gmail.com>
2019-10-09 15:23:27 +0200

    init commit

---
+++ R/compilePdfs.R
@@ -0,0 +1,23 @@
+
+#' compilePdfs
+#'
+#' @param targetDir 
+#' @param context 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+compilePdfs <- function(targetDir = "./tmp", context = "anal") {
+  
+  files <- file.info(list.files(targetDir, pattern = paste0("^", context),
+                                full.names = TRUE))
+  
+  t <- character()
+  
+  for (file in rownames(files)) {
+    t <- c(t, pdf_text(file))
+  }
+  
+  cat(t)
+}
